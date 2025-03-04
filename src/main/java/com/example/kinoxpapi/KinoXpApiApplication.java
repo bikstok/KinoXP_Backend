9c5cf6905baa9df512cea255dad06527e962989e
JensKristianRuben <Jenskristian@gmail.dk>
2025-03-04 14:25:14 +0100

    Initial Commit

---
+++ src/main/java/com/example/kinoxpapi/KinoXpApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.kinoxpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KinoXpApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KinoXpApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/kinoxpapi/KinoXpApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.kinoxpapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KinoXpApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
