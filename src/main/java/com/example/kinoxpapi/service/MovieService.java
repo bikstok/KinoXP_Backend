41e37cfba6e2afc9a79880b7ea899943a94acf3b
JensKristianRuben <Jenskristian@gmail.dk>
2025-03-07 11:34:11 +0100

    Oprettelse af endpoint til sletning af film

--- src/main/java/com/example/kinoxpapi/service/MovieService.java
+++ src/main/java/com/example/kinoxpapi/service/MovieService.java
@@ -1,7 +1,29 @@
 package com.example.kinoxpapi.service;
 
+import com.example.kinoxpapi.model.Movie;
+import com.example.kinoxpapi.repository.MovieRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Optional;
 
 @Service
 public class MovieService {
+
+    @Autowired
+    MovieRepository movieRepository;
+
+    public Optional<Movie> deactivateMovie(int movieId){
+        Optional<Movie> optionalMovie = movieRepository.findById(movieId);
+
+        if (optionalMovie.isPresent() && optionalMovie.get().isInRotation()){
+            optionalMovie.get().setInRotation(false);
+
+            movieRepository.save(optionalMovie.get());
+
+            return optionalMovie;
+        }
+
+        return optionalMovie;
+    }
 }
